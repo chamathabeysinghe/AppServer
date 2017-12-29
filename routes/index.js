6d42d50fd406153326f82d0859a61907217cf1ce
chamathabeysinghe <abeysinghechamath@gmail.com>
2017-12-29 11:10:19 +0530

    Add login and dashboard pages

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('jp', {title: 'Express'});
 });
 
+router.get('/login', function (req, res, next) {
+    res.render('login');
+});
+
+router.get('/dashboard', function (req, res, next) {
+    res.render('dashboard');
+});
 module.exports = router;
